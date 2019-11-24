bf745dbbb1a5d1875a8631d8c441726f9e2fca56
Administrator <1581404823@qq.com>
2019-11-24 16:44:07 +0800

    提交

---
+++ src/main/java/com/tedu/cn/Hello.java
@@ -0,0 +1,10 @@
+package com.tedu.cn;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("*********************");
+        System.out.println("*****************");
+        System.out.println("@@@@@@@@@@@@@@@@@@");
+        System.out.println("&&&&&&&&&&&&&&&&&&&");
+    }
+}
